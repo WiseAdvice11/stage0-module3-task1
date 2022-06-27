6e877a30094b1ad729680954e801e7a8b5dc664f
Roamn Pivovarov <breweroff@gmail.com>
2022-06-27 23:29:33 +0300

    fix

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -3,6 +3,8 @@ package lang.print.gaps.task1;
 public class PrintSentenceApp {
     public static void main(String[] args) {
 
-        System.out.println( "This \n is \n my \n first \n java \n program");
+        System.out.println( "This is my first java program\n");
+        System.out.println("This is my first Java program\n");
+
     }
 }
