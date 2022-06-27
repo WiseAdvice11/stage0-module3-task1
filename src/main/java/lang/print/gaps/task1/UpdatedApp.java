3d6e941ebaf897df70bb979eecf1a1f5ebd61b5e
Roamn Pivovarov <breweroff@gmail.com>
2022-06-27 23:10:09 +0300

    fix

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("updated");
+
+        System.out.println("I'm\nupdated" + "\n" + "can \nbe \ncompiled.");
     }
 }
+
